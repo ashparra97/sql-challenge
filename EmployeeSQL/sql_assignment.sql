57566d0f67762e53ec82ef054f79a857465961e3
ashparra97 <ashparra97@arizona.edu>
2021-05-12 19:09:53 -0700

    Create sql_assignment.sql

---
+++ EmployeeSQL/sql_assignment.sql
@@ -0,0 +1,43 @@
+-- design the tables to hold data in the CSVs
+--create table schemas for each of 6 csv files 
+	-- specify datatypes 
+	-- specify primary keys 
+	-- specify foreign keys 
+	-- specify other constraints
+-- import the CSVs into a SQL database 
+-- : 
+/*
+List the following details of each employee
+	employee number
+	last name
+	first name 
+	sex
+	salary
+
+List first name, last name, and hire date for employees who were hired in 1986
+
+
+List the manager of each department with the following information: 
+	department number
+	department name
+	the manager's employee number
+	last name
+	first name
+
+
+List the department of each employee with the following information: 
+	employee number
+	last name
+	first name
+	department name
+
+List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.*/
